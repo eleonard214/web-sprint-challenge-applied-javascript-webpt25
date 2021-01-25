07b7e1f0e46210f9130c547df029a8afa86107bb
Erica Leonard <elynn1979@live.com>
2021-01-24 22:22:45 -0500

    I am so lost

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,23 @@
 const Header = (title, date, temp) => {
+  const header= document.createElement('div')
+  header.classList.add('header')
+
+  const date = document.createElement('span')
+  date.classList.add('date')
+  date.textContent="January 6, 2021"
+  header.appendChild('date')
+
+  const title= document.createElement('h1')
+  title.textContent="Lambda Times"
+  header.appendChild('h1')
+
+  const temp =document.createElement('span')
+  header.classList.add('temp')
+  temp.textContent="26°"
+  header.appendChild('temp')
+
+  return header
+}
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -14,6 +33,8 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+  selector.appendChild('div .header')}
+  
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
